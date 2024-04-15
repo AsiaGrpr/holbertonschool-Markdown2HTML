089542bbda70878865dbbcf39185116db9e3b76e
asia groupierre <groupierre.asia@gmail.com>
2024-04-15 16:47:27 +0200

    test to fix output

--- markdown2html.py
+++ markdown2html.py
@@ -14,7 +14,7 @@ def convert_markdown_to_html(markdown_file, output_file):
 
     # Check if markdown file exists
     if not os.path.isfile(sys.argv[1]):
-        print("Missing {}".format(sys.argv[1]), file=sys.stderr)
+        print("Missing {}\n".format(sys.argv[1]), file=sys.stderr)
         sys.exit(1)
 
 
@@ -22,7 +22,7 @@ if __name__ == "__main__":
 
     # Check for correct number of arguments
     if len(sys.argv) < 3:
-        print("Usage: ./markdown2html.py README.md README.html",
+        print("Usage: ./markdown2html.py README.md README.html\n",
               file=sys.stderr)
         sys.exit(1)
 
