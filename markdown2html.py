9df26c5c693c5693fe95a409383447145516743f
asia groupierre <groupierre.asia@gmail.com>
2024-04-15 16:01:53 +0200

    task 0 check

--- markdown2html.py
+++ markdown2html.py
@@ -4,7 +4,7 @@ import sys
 import os
 
 
-def main():
+def main(markdown_file, output_file):
     """Converts a Markdown file to HTML.
 
     Expects two arguments:
@@ -12,15 +12,23 @@ def main():
       2. Output HTML file name
     """
 
-    # Check for correct number of arguments
-    if len(sys.argv) < 2:
-        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
-        sys.exit(1)
-
     # Check if markdown file exists
     if not os.path.isfile(sys.argv[1]):
         print("Missing {}".format(sys.argv[1]), file=sys.stderr)
         sys.exit(1)
+
+    # Check for correct number of arguments
+    if len(sys.argv) < 3:
+        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+        sys.exit(1)
+
+    # Extract name file
+    markdown_file = sys.argv[1]
+    output_file = sys.argv[2]
+
+    # Convert Markdown to HTML
+    main(markdown_file, output_file)
+
     # Successful execution, no output message needed
     sys.exit(0)
 
