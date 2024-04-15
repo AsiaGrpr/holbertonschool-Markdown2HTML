08749717701e7938ecac04881f3df9b716ba7db5
asia groupierre <groupierre.asia@gmail.com>
2024-04-15 15:36:10 +0200

    repo and readme, script test - first commit

---
+++ markdown2html.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+import sys
+import os
+
+
+def main():
+    """Converts a Markdown file to HTML.
+
+    Expects two arguments:
+      1. Input Markdown file name
+      2. Output HTML file name
+    """
+
+    # Check for correct number of arguments
+    if len(sys.argv) < 3:
+        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+        sys.exit(1)
+
+    # Check if markdown file exists
+    if not os.path.isfile(sys.argv[1]):
+        print("Missing {}".format(sys.argv[1]), file=sys.stderr)
+        sys.exit(1)
+    # Successful execution, no output message needed
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
